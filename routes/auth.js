5e325f17bd5d76be8a50d3242e99b33e20c5ab66
hellcat <anondsa349@gmail.com>
2026-04-18 23:23:35 +0530

    Added auth Middleware and token synchronization

---
+++ routes/auth.js
@@ -0,0 +1,152 @@
+require('dotenv').config()
+
+const express = require('express')
+const bcrypt = require('bcrypt')
+const { PrismaClient } = require('@prisma/client')
+const { PrismaPg } = require('@prisma/adapter-pg')
+const ratelimit = require('express-rate-limit')
+const jwt = require('jsonwebtoken')
+
+const app = express()
+const router = express.Router()
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
+
+app.use(express.json())
+
+const limiter = ratelimit({
+    windowMs: 10 * 60 * 1000,
+    limit: 5,
+    statusCode: 429,
+    message: "429 Too many requests."
+})
+
+// POST - Register endpoint
+
+router.post('/register', limiter, async (req, res) => {
+    const { email, username, password } = req.body
+
+    if (!email || !username || !password) {
+        return res.status(400).send("All fields are mandatory.")
+    }
+
+    try {
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        if (!passwordRegex.test(password)) {
+            return res.status(400).json({
+                error: "Password must be 8+ chars with uppercase, lowercase, number, and special character"
+            })
+        }
+
+        if (password.length > 16) {
+            return res.status(400).json({
+                error: "Password should not be greater than 16 digits"
+            })
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                error: "Invalid email."
+            })
+        }
+
+        const existingEmail = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (existingEmail) {
+            return res.status(400).json({
+                message: "Error creating user",
+                error: "Email already exists."
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await prisma.user.create({
+            data: {
+                email,
+                username,
+                password: hashedPassword,
+                datentime: new Date()
+            }
+        })
+        res.json({
+            message: "User created",
+            id: user.id,
+            email: email,
+            username: user.username,
+            datentime: user.datentime
+        })
+    }
+
+    catch (err) {
+        if (err.code === 'P2002') {
+            return res.status(400).json({
+                error: "User already exists"
+            })
+        }
+
+        console.log(err)
+        res.status(500).send("An error has occurred.")
+    }
+})
+
+// POST - Login endpoint
+
+router.post('/login', limiter, async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.status(400).json({
+            error: "All fields are required."
+        })
+    }
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (!user) {
+            return res.status(400).json({
+                error: "Invalid credentials."
+            })
+        }
+
+        const isValid = await bcrypt.compare(password, user.password)
+
+        if (!isValid) {
+            return res.status(400).json({
+                error: "Invalid credentials."
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user.id,
+                email: user.email
+            },
+
+            process.env.JWT_SECRET_KEY,
+
+            {
+                expiresIn: '1h'
+            }
+        )
+
+        res.json({
+            message: "User logged in successfully.",
+            token
+        })
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            error: "An error occured."
+        })
+    }
+})
+
+app.use(limiter)
+
+module.exports = router
---
+++ routes/profile.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const authMiddleware = require('../middleware/authMiddleware')
+const app = express()
+const router = express.Router()
+
+app.use(express.json())
+
+router.get('/profile',authMiddleware, (req, res) => {
+    res.json({
+        message: "User has been authenticated",
+        user: req.user
+    })
+})
+
+module.exports = router
