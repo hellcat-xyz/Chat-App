33864e5466dcd962458951d53eea578871b0dd62
Dharamvir Singh <dharam3k@gmail.com>
2026-04-22 22:34:27 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './App.css';
 import Cookies from 'universal-cookie';
 import ChannelListContainer from './components/ChannelListContainer.jsx'
 import ChannelContainer from './components/ChannelContainer.jsx'
+import Auth from './components/Auth.jsx';
 import ChatClient from "./client/chatClient.js";
 import { Chat } from "./context/Chat.jsx";
 
 const client = new ChatClient("http://localhost:5000");
+const authToken = false;
+// need auth token
 
 const App = () => {
+  if(!authToken) return <Auth />
   return (
     <div className='app__wrapper'>
       <Chat client={client}>
